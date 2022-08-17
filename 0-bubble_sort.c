78d78c9743326c0a904b7e7edc0d7b6de3fce7a7
Perpy-del <perpetualmeninwa@gmail.com>
2022-08-17 18:15:52 +0000

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,48 +1,37 @@
 #include "sort.h"
 
 /**
- * sort_swap - Swap the two integers in the array.
- * @x: The first integer.
- * @y: The second integer.
- */
-
-void sort_swap(int *x, int *y)
-{
-	int temp;
-
-	temp = *x
-	*x = *y;
-	*y = temp;
-}
-
-/**
  * bubble_sort - Function that sorts an array of integers in ascending order.
  * @array - The array of integers.
  * @size - The size of the array.
  *
  * Description: Print the array each time there is a swap.
  */
-
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j = size;
-	bool bubble = false;
+	size_t i, j, sorted = 0;
+	int temp;
 
-	if (array == NULL || size < 2)
+	if (size < 2)
 		return;
 
-	while (bubble == false)
+	/* iteration over the list. */
+	for (i = 0; i < size; i++)
 	{
-		bubble == true;
-		for (i = 0; i < j - 1, i++)
+		sorted = 0;
+
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[i] > array [i + 1])
+			if(array[j] > array[j + 1])
 			{
-				sort_swap(array + i, array + i + 1);
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				sorted = 1;
 				print_array(array, size);
-				bubble = false;
 			}
 		}
-		j--;
+		if (sorted == 0)
+			break;
 	}
 }
