85ac61a34fa1e16614c6bfbba38d7b5d023b8d78
Perpy-del <perpetualmeninwa@gmail.com>
2022-08-17 18:30:22 +0000

    bubbly sort notation

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -4,8 +4,6 @@
  * bubble_sort - Function that sorts an array of integers in ascending order.
  * @array - The array of integers.
  * @size - The size of the array.
- *
- * Description: Print the array each time there is a swap.
  */
 void bubble_sort(int *array, size_t size)
 {
